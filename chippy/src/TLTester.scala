28bd10a7b0586524d8912051b5ecc1530a5c1ae6
Rohan Kumar <rohankumar@berkeley.edu>
2026-02-16 08:06:24 -0800

    clean up TLTester API

--- chippy/src/TLTester.scala
+++ chippy/src/TLTester.scala
@@ -2,11 +2,20 @@ package edu.berkeley.cs.chippy
 
 import chisel3._
 import chisel3.util._
+import chisel3.experimental.BundleLiterals
+import chisel3.experimental.VecLiterals._
+import chisel3.simulator.{AnySimulatedModule, PeekPokeAPI}
+import chisel3.experimental.{SourceInfo, SourceLine}
 import org.chipsalliance.diplomacy.lazymodule._
 import org.chipsalliance.cde.config.{Parameters, Field}
 import freechips.rocketchip.diplomacy.IdRange
 import freechips.rocketchip.tilelink._
 import freechips.rocketchip.util._
+import firrtl.options.StageUtils.dramaticMessage
+import svsim._
+
+import scala.util.control.NoStackTrace
+import scala.language.implicitConversions
 
 case class TLTesterParams(
     maxInflight: Int = 1,
@@ -29,8 +38,60 @@ class TLTesterResp(params: TLTesterParams) extends Bundle {
 }
 
 class TLTesterIO(params: TLTesterParams) extends Bundle {
+  import chisel3.simulator.PeekPokeAPI._
   val req = Flipped(new DecoupledIO(new TLTesterReq(params)))
   val resp = new DecoupledIO(new TLTesterResp(params))
+
+  final def write(
+      clock: Clock,
+      addr: UInt,
+      data: UInt,
+      timeout: Int = 1000
+  )(implicit sourceInfo: SourceInfo): Unit = {
+    op(clock, addr, data, true.B, timeout = timeout)
+    clock.step()
+  }
+
+  final def read(
+      clock: Clock,
+      addr: UInt,
+      timeout: Int = 1000
+  )(implicit sourceInfo: SourceInfo): UInt = {
+    op(clock, addr, 0.U, false.B, timeout = timeout)
+    val out = resp.bits.data.peek()
+    clock.step()
+    out
+  }
+
+  final def expect(
+      clock: Clock,
+      addr: UInt,
+      expected: UInt,
+      timeout: Int = 1000,
+      message: String = ""
+  )(implicit sourceInfo: SourceInfo): Unit = {
+    op(clock, addr, 0.U, false.B, timeout = timeout)
+    resp.bits.data.expect(expected, message)
+    clock.step()
+  }
+
+  final def op(
+      clock: Clock,
+      addr: UInt,
+      data: UInt,
+      is_write: Bool,
+      timeout: Int = 1000
+  )(implicit sourceInfo: SourceInfo): Unit = {
+    resp.ready.poke(true.B)
+    clock.stepUntil(req.ready, 1, timeout)
+    req.ready.expect(true.B, "timeout waiting for request to be ready")
+    req.valid.poke(true.B)
+    req.bits.addr.poke(addr)
+    req.bits.data.poke(data)
+    req.bits.is_write.poke(is_write)
+    clock.stepUntil(resp.valid, 1, timeout)
+    req.ready.expect(true.B, "timeout waiting for response to be valid")
+  }
 }
 
 class TLTester(params: TLTesterParams, beatBytes: Int)(implicit p: Parameters)
