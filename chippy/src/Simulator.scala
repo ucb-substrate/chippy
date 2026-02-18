2a353b3360de146748dd9906cfe10d1c208dcc39
Rohan Kumar <rohankumar@berkeley.edu>
2026-02-18 12:14:08 -0800

    get binary simulations working

--- chippy/src/Simulator.scala
+++ chippy/src/Simulator.scala
@@ -11,16 +11,14 @@ object Simulator {
       sourceFilesList: Path,
       simScript: Path
   )
-  def verilatorCmdFiles(
-      topModule: String,
-      workDir: Path,
-      sourceFiles: Seq[Path] = Seq.empty,
-      incDirs: Seq[Path] = Seq.empty
-  ) = {
-    os.makeDir.all(workDir)
-    val sourceFilesList = workDir / "sourceFiles.F"
-    val cmdFile = workDir / "simulation.sh"
-    val src = new LineWriter(sourceFilesList.toString)
+
+  /** Writes a source file list, prefixing .h files with a `-FI` flag to force inclusion.
+   *
+   *  Intended to be included in a Verilator invocation using `-F`
+   */
+  def writeVerilatorSourceFilesList(path: Path, sourceFiles: Seq[Path]) {
+    os.makeDir.all(path / os.up)
+    val src = new LineWriter(path.toString)
     try {
       for (file <- sourceFiles) {
         if (file.last.endsWith(".h")) {
@@ -32,58 +30,102 @@ object Simulator {
     } finally {
       src.close()
     }
-    val cmd = new LineWriter(cmdFile.toString)
+  }
+
+  /** Writes a script that compiles source files using Verilator then runs the simulation binary. */
+  def writeVerilatorSimScript(
+      path: Path,
+      topModule: String,
+      sourceFilesList: Path,
+      incDirs: Seq[Path] = Seq.empty,
+      verilatorCmd: String = "verilator",
+      enableAssertions: Boolean = true,
+      enableTiming: Boolean = true,
+      maxNumWidth: Option[Int] = Some(1048576),
+      parallelJobs: Option[Int] = Some(0),
+      timescale: Option[String] = Some("1ns/100ps"),
+      vpi: Boolean = true,
+      noFatal: Boolean = true,
+      // TODO: Figure out how to do escaping more cleanly
+      cflags: Option[String] = Some("\"$CXXFLAGS -O3 -std=c++17 -DVERILATOR -I$RISCV/include\""),
+      ldflags: Option[String] = Some("\"$LDFLAGS -L$RISCV/lib -Wl,-rpath,$RISCV/lib -lriscv -lfesvr\""),
+      preprocessorDefines: Seq[String] = Seq(
+        "layer$Verification$Assert$Temporal",
+        "layer$Verification$Assume$Temporal",
+        "layer$Verification$Cover$Temporal",
+        "VERILATOR"
+      ),
+      additionalCompilationLines: Seq[String] = Seq.empty,
+      additionalSimulationLines: Seq[String] = Seq.empty,
+  ) = {
+    os.makeDir.all(path / os.up)
+    val cmd = new LineWriter(path.toString)
     try {
       cmd("#!/bin/bash")
-      cmd("verilator \\")
+      cmd(verilatorCmd, " \\")
       cmd(" --cc \\")
       cmd(" --exe \\")
       cmd(" --build \\")
+      cmd(" --main \\")
       cmd(" -o ../simulation \\")
-      cmd("  --top-module ", topModule, " \\")
+      cmd(" --top-module ", topModule, " \\")
       cmd(" --Mdir verilated-sources \\")
-      cmd(" --assert \\")
-      cmd(" --timing \\")
-      cmd(" --max-num-width 1048576 \\")
-      cmd(" -j 0 \\")
+      if (enableAssertions) {
+        cmd(" --assert \\")
+      }
+      if (enableTiming) {
+        cmd(" --timing \\")
+      }
+      maxNumWidth match {
+        case Some(v) => cmd(s" --max-num-width $v \\")
+        case None =>
+      }
+      parallelJobs match {
+        case Some(v) => cmd(s" -j $v \\")
+        case None =>
+      }
       for (dir <- incDirs) {
         cmd("  +incdir+", dir.toString, " \\")
       }
-      cmd(" --timescale 1ns/100ps \\")
-      cmd(" --trace \\")
-      cmd(" --trace-underscore \\")
-      cmd(" --trace-structs \\")
-      cmd(" --trace-max-array 1024 \\")
-      cmd(" --trace-max-width 1024 \\")
-      cmd(" --trace-depth 1024 \\")
-      cmd(" -Wno-fatal \\")
-      cmd(
-        " -CFLAGS \"-std=c++17 -DSVSIM_ENABLE_VERILATOR_SUPPORT -DSVSIM_VERILATOR_TRACE_ENABLED  -I$RISCV/include\" \\"
-      )
-      cmd(" -LDFLAGS \"-L$RISCV/lib -lfesvr\" \\")
-      cmd(" -F ", sourceFilesList.toString)
-      // "+define+ASSERT_VERBOSE_COND=!svsimTestbench.reset",
-      // "+define+PRINTF_COND=!svsimTestbench.reset",
-      // "+define+STOP_COND=!svsimTestbench.reset",
-      // "+define+layer$Verification$Assert$Temporal",
-      // "+define+layer$Verification$Assume$Temporal",
-      // "+define+layer$Verification$Cover$Temporal",
-      // "+define+RANDOMIZE_REG_INIT",
-      // "+define+RANDOMIZE_MEM_INIT",
-      // "+define+RANDOMIZE_DELAY=1",
-      // "+define+RANDOM=$urandom",
-      // "+define+SVSIM_ENABLE_VCD_TRACING_SUPPORT",
+      timescale match {
+        case Some(v) => cmd(s" --timescale $v \\")
+        case None =>
+      }
+      if (vpi) {
+        cmd(" --vpi \\")
+      }
+      for (define <- preprocessorDefines) {
+        cmd(s" +define+$define \\")
+      }
+      if (noFatal) {
+        cmd(" -Wno-fatal \\")
+      }
+      cflags match {
+        case Some(v) => cmd(s" -CFLAGS $v \\")
+        case None =>
+      }
+      ldflags match {
+        case Some(v) => cmd(s" -LDFLAGS $v \\")
+        case None =>
+      }
+      cmd(" -F ", sourceFilesList.toString, if (additionalCompilationLines.isEmpty) { "" } else { " \\" })
+      if (additionalCompilationLines.nonEmpty) {
+        cmd(" ", additionalCompilationLines.mkString(" \\\n "))
+      }
+      if (additionalSimulationLines.isEmpty) {
+        cmd("./simulation")
+      } else {
+        cmd("./simulation \\")
+        cmd(" ", additionalSimulationLines.mkString(" \\\n "))
+      }
     } finally {
       cmd.file.setExecutable(true)
       // `BufferedWriter` closes the underlying `FileWriter` when closed.
       cmd.close()
     }
-    VerilatorCmdFiles(
-      sourceFilesList,
-      cmdFile
-    )
   }
 
+  /** Finds source files within a given source directory with the given file extensions. */
   def getSourceFiles(
       sourceDir: Path,
       fileExtensions: Seq[String] = Seq(".v", ".sv", ".cc", ".h", ".vams")
@@ -95,8 +137,7 @@ object Simulator {
   }
 }
 
-/** A micro-DSL for writing files.
-  */
+/** A micro-DSL for writing files. */
 private class LineWriter(val path: String) {
   val file = new File(path)
   private val wrapped = new BufferedWriter(new FileWriter(file, false))
--- examples/sky130_chip/digital_chip/test/src/TopSpec.scala
+++ examples/sky130_chip/digital_chip/test/src/TopSpec.scala
@@ -5,7 +5,7 @@ import chisel3.util._
 import chisel3.experimental.BundleLiterals._
 
 import org.scalatest.funspec.AnyFunSpec
-import edu.berkeley.cs.chippy.{ChippyStage, Simulator}
+import edu.berkeley.cs.chippy.Simulator
 import org.chipsalliance.diplomacy.lazymodule._
 import org.chipsalliance.diplomacy._
 import org.chipsalliance.cde.config.Parameters
@@ -30,20 +30,45 @@ import java.nio.file.Paths
 import os.RelPath
 import os.Path
 
+class SimTop extends RawModule {
+  val driver = Module(new TestDriver)
+  val harness = Module(new TestHarness)
+  harness.io.reset := driver.reset
+  driver.success := harness.io.success
+}
+
 class TestDriver extends ExtModule {
+  val success = IO(Input(Bool()))
+  val reset = IO(Output(Bool()))
   setInline(
     "TestDriver.v",
     """module TestDriver(
+      | input success,
+      | output reset
       |);
-      | $finish
+      | initial begin
+      |   $display("Resetting chip for 10 ns");
+      |   reset = 1'b1;
+      |   #10;
+      |   reset = 1'b0;
+      |   $display("Running test for 1000 ns");
+      |   #1000000000;
+      |   $display("Test timed out!");
+      |   $fatal;
+      | end
+      | always @(posedge success) begin
+      |   $display("Test completed successfully.");
+      |   $finish;
+      | end
       |endmodule
     """.stripMargin
   )
 }
 
-class TestHarness extends Module {
+class TestHarness extends RawModule {
   val io = IO(new Bundle {
     val success = Output(Bool())
+    val reset = Input(Bool())
   })
 
   implicit val p: Parameters = new DigitalChipConfig
@@ -56,51 +81,51 @@ class TestHarness extends Module {
   source.io.gate := false.B
   digitalClock := source.io.clk
 
-  val chiptop_lazy = LazyModule(new DigitalChipTop)
-  val chiptop = Module(chiptop_lazy.module)
-  chiptop.io.clock := digitalClock
-  chiptop.io.reset := reset.asAsyncReset
+  withClockAndReset(digitalClock, io.reset) {
+    val chiptop_lazy = LazyModule(new DigitalChipTop)
+    val chiptop = Module(chiptop_lazy.module)
+    chiptop.io.clock := digitalClock
+    chiptop.io.reset := io.reset.asAsyncReset
 
-  val div =
-    (digitalFreqMHz.toDouble * 1000000 / chiptop.io.uart.c.initBaudRate.toDouble).toInt
-  UARTAdapter.connect(Seq(chiptop.io.uart), div, false)
+    val div =
+      (digitalFreqMHz.toDouble * 1000000 / chiptop.io.uart.c.initBaudRate.toDouble).toInt
+    UARTAdapter.connect(Seq(chiptop.io.uart), div, false)
 
-  io.success := false.B
+    io.success := false.B
 
-  val dtm_success = WireInit(false.B)
-  when(dtm_success) { io.success := true.B }
-  val jtag_wire = Wire(new JTAGIO)
-  jtag_wire.TDO.data := chiptop.io.jtag.TDO
-  jtag_wire.TDO.driven := true.B
-  chiptop.io.jtag.TCK := jtag_wire.TCK
-  chiptop.io.jtag.TMS := jtag_wire.TMS
-  chiptop.io.jtag.TDI := jtag_wire.TDI
-  val jtag = Module(new SimJTAG(tickDelay = 3))
-  jtag.connect(
-    jtag_wire,
-    digitalClock,
-    reset.asBool,
-    ~(reset.asBool),
-    dtm_success
-  )
-
-  val driver = Module(new TestDriver)
-
-  chiptop.io.serial_tl.clock_in := digitalClock
-  withClock(digitalClock) {
-    val ram = Module(
-      LazyModule(
-        new SerialRAM(chiptop_lazy.system.serdessers(0), p(SerialTLKey)(0))(
-          chiptop_lazy.system.serdessers(0).p
-        )
-      ).module
+    val dtm_success = WireInit(false.B)
+    when(dtm_success) { io.success := true.B }
+    val jtag_wire = Wire(new JTAGIO)
+    jtag_wire.TDO.data := chiptop.io.jtag.TDO
+    jtag_wire.TDO.driven := true.B
+    chiptop.io.jtag.TCK := jtag_wire.TCK
+    chiptop.io.jtag.TMS := jtag_wire.TMS
+    chiptop.io.jtag.TDI := jtag_wire.TDI
+    val jtag = Module(new SimJTAG(tickDelay = 3))
+    jtag.connect(
+      jtag_wire,
+      digitalClock,
+      io.reset,
+      ~(io.reset),
+      dtm_success
     )
-    ram.io.ser.in <> chiptop.io.serial_tl.out
-    chiptop.io.serial_tl.in <> ram.io.ser.out
 
-    val success =
-      SimTSI.connect(ram.io.tsi, digitalClock, reset)
-    when(success) { io.success := true.B }
+    chiptop.io.serial_tl.clock_in := digitalClock
+    withClock(digitalClock) {
+      val ram = Module(
+        LazyModule(
+          new SerialRAM(chiptop_lazy.system.serdessers(0), p(SerialTLKey)(0))(
+            chiptop_lazy.system.serdessers(0).p
+          )
+        ).module
+      )
+      ram.io.ser.in <> chiptop.io.serial_tl.out
+      chiptop.io.serial_tl.in <> ram.io.ser.out
+
+      val success =
+        SimTSI.connect(ram.io.tsi, digitalClock, io.reset)
+      when(success) { io.success := true.B }
+    }
   }
 }
 
@@ -130,27 +155,33 @@ class DigitalChipTopSpec extends AnyFunSpec {
       implicit val p = new DigitalChipConfig
       val sourceDir = testRoot / "Top_should_run_hello_riscv/src"
       val workDir = testRoot / "Top_should_run_hello_riscv/work"
-      ChiselStage.emitSystemVerilogFile(
-        new TestHarness,
-        args = Array(
-          "--target-dir",
-          sourceDir.toString
-        )
-      )
+       ChiselStage.emitSystemVerilogFile(
+         new SimTop,
+         args = Array(
+           "--target-dir",
+           sourceDir.toString
+         )
+       )
       val sourceFiles = Simulator.getSourceFiles(sourceDir)
 
-      val cmdFiles = Simulator.verilatorCmdFiles(
-        "TestDriver",
-        workDir,
-        sourceFiles = sourceFiles,
-        incDirs = os.walk(sourceDir).filter(os.isDir) ++ Seq(sourceDir)
+      val sourceFilesList = workDir / "sourceFiles.F"
+      val simScript = workDir / "simulate.sh"
+      
+      Simulator.writeVerilatorSourceFilesList(sourceFilesList, sourceFiles)
+
+      Simulator.writeVerilatorSimScript(
+        simScript,
+        "SimTop",
+        sourceFilesList,
+        incDirs = os.walk(sourceDir).filter(os.isDir) ++ Seq(sourceDir),
+        additionalSimulationLines = Seq("+permissive", "+verbose", "+permissive-off", "/scratch/rohankumar/chipyard/tests/build/hello.riscv")
       )
 
       os.proc(
         "/bin/bash",
         "-c",
-        cmdFiles.simScript
-      ).call(cwd = workDir)
+        simScript
+      ).call(stdout = os.Inherit, stderr = os.Inherit, cwd = workDir)
 
     }
   }
