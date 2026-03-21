186eda1fa0375a26c6b3cd8d156f5dc348f85298
Rohan Kumar <rohankumar@berkeley.edu>
2026-03-20 17:21:23 -0700

    fix SimTSI issues

--- chippy/resources/csrc/SimTSI.cc
+++ chippy/resources/csrc/SimTSI.cc
@@ -4,19 +4,23 @@
 #include <string>
 #include "testchip_tsi.h"
 
-testchip_tsi_t* tsi = NULL;
+int next_id = 0;
+std::map<int, testchip_tsi_t*> tsis;
 
-extern "C" void tsi_init(svOpenArrayHandle argv, bool can_have_loadmem)
+extern "C" int tsi_init(int argc, svOpenArrayHandle argv, unsigned char can_have_loadmem)
 {
-    int left = svLeft(argv,1);
-    int right = svRight(argv,1);
-    char** argv_actual = (char **) svGetArrElemPtr1(argv, 0);;
-
-    // TODO: We should somehow inspect whether or not our backing memory supports loadmem, instead of unconditionally setting it to true
-    tsi = new testchip_tsi_t(right - left + 1, argv, can_have_loadmem);
+    int id = next_id++;
+    char* argv_actual[argc + 1];
+    for (int i = 0; i < argc; i++) {
+        argv_actual[i] = *(char**)svGetArrElemPtr1(argv, i);
+    }
+    argv_actual[argc] = NULL;
+    tsis[id] = new testchip_tsi_t(argc, argv_actual, can_have_loadmem);
+    return id;
 }
 
 extern "C" int tsi_tick(
+                        int id,
                         unsigned char out_valid,
                         unsigned char *out_ready,
                         int out_bits,
@@ -29,12 +33,8 @@ extern "C" int tsi_tick(
     bool in_fire = *in_valid && in_ready;
     bool in_free = !(*in_valid);
 
-    if (tsi == NULL) {
-        // TODO: We should somehow inspect whether or not our backing memory supports loadmem, instead of unconditionally setting it to true
-        tsi = new testchip_tsi_t(argc, argv, can_have_loadmem);
-    }
-
-    testchip_tsi_t* tsi = tsis[chip_id];
+    testchip_tsi_t* tsi = tsis[id];
+    
     tsi->tick(out_valid, out_bits, in_ready);
     tsi->switch_to_host();
 
