f7ba75ea615ad3439c70557c5e1196adda785b5b
rohankudchadker <rohankudchadker@berkeley.edu>
2026-05-01 15:52:10 -0700

    use latest version of actions checkout and setup-java

--- chippy-test/test/src/Main.scala
+++
@@ -1,8 +0,0 @@
-import org.chipsalliance.diplomacy.lazymodule.LazyModule
-
-object Main {
-  def main(args: Array[String]): Unit = {
-    val x: Option[LazyModule] = None
-    println("Successfully imported diplomacy: " + x)
-  }
-}
